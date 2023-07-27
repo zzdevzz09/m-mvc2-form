8dbdef91f9660f487f0ecd39d00ad392e77a6701
Jeyun Park <jy095201@gmail.com>
2023-07-27 13:05:00 +0900

    check box

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -2,10 +2,14 @@ package hello.itemservice.domain.item;
 
 public enum ItemType {
 	BOOK("도서"), FOOD("음식"), ETC("기타");
-	
-	private final String desription;
-	
-	private ItemType(String description) {
-		this.desription = description;
+
+	private final String description;
+
+	ItemType(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 }
--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,7 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,6 +22,21 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+    
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+    	Map<String, String> regions = new LinkedHashMap<>();
+    	regions.put("SEOUL", "서울");
+    	regions.put("BUSAN", "부산");
+    	regions.put("JEJU", "제주");
+    	
+    	return regions;
+    }
+    
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemType() {
+    	return ItemType.values();
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -37,12 +55,22 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
     	model.addAttribute("item", new Item());
+    	
+    	Map<String, String> regions = new LinkedHashMap<>();
+    	regions.put("SEOUL", "서울");
+    	regions.put("BUSAN", "부산");
+    	regions.put("JEJU", "제주");
+    	model.addAttribute(regions);
+    	
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
     	log.info("item.open={}", item.getOpen());
+    	log.info("item.regions={}", item.getRegions());
+    	log.info("item.itemType={}", item.getItemType());
+    	
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
